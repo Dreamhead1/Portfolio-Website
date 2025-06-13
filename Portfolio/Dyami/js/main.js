8c25cc5624f2be4c85c7e37c188f2abfa6135ebd
Dreamhead1 <dreamhead.citizen@gmail.com>
2025-06-13 13:08:02 -0600

    wowzers

---
+++ Portfolio/Dyami/js/main.js
@@ -0,0 +1,26 @@
+var img = document.getElementById ("myIMG");
+var imgMVMT = document.getElementById ("myIMG").src;
+var abstractSoul = document.getElementById ("1");
+var acidBath = document.getElementById ("2");
+var Cyclops = document.getElementById ("3");
+var DMT = document.getElementById ("4");
+var egoDeath = document.getElementById ("5");
+var Falling = document.getElementById ("6");
+var hungryDog = document.getElementById ("7");
+var idDissolution = document.getElementById ("8");
+var Lovers = document.getElementById ("9");
+var maskFlower = document.getElementById ("10");
+var Monkey = document.getElementById ("11");
+var deathGod = document.getElementById ("12");
+var paintingShadow = document.getElementById ("13");
+var penguinHead = document.getElementById ("14");
+var potHead = document.getElementById ("15");
+var Protection = document.getElementById ("16");
+var pullingMe = document.getElementById ("17");
+var ratbirdMan = document.getElementById ("18");
+var shadowSplit = document.getElementById ("19");
+var snakeSkull = document.getElementById ("20");
+var theSheriff = document.getElementById ("21");
+var Tiger = document.getElementById ("22");
+
+
